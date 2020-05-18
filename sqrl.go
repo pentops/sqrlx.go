20cb74354ac42e4f2aae4d62e541dd3bbea296df
Damien Whitten <dw@chaos404.com>
2020-05-18 20:08:02 +1000

    Add Retry Loops

--- sqrl.go
+++ sqrl.go
@@ -8,14 +8,33 @@ import (
 	sq "github.com/elgris/sqrl"
 )
 
-type Connection interface {
-	Queryer
-	BeginTx(context.Context, *sql.TxOptions) (*sql.Tx, error)
+// QueryError is thrown by all exec and query commands to wrap the driver error.
+// It includes the statement causing the error
+type QueryError struct {
+	cause     error
+	Statement string
 }
 
+// Cause gives the driver error which was thrown
+func (err QueryError) Cause() error {
+	return err.cause
+}
+
+// Error is the cause error + the statement causing it
+func (err QueryError) Error() string {
+	return err.cause.Error() + " `" + err.Statement + "` "
+}
+
+// Queryer runs database queries
 type Queryer interface {
 	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
 	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+}
+
+// Connection is Queryer + Begin
+type Connection interface {
+	Queryer
+	BeginTx(context.Context, *sql.TxOptions) (*sql.Tx, error)
 }
 
 // ColumnType is implemented by *sql.ColumnType
@@ -47,6 +66,7 @@ type Wrapper struct {
 type QueryWrapper struct {
 	db                Queryer
 	placeholderFormat sq.PlaceholderFormat
+	RetryCount        int
 }
 
 var _ Transaction = Wrapper{}
@@ -57,6 +77,7 @@ func New(conn Connection, placeholder sq.PlaceholderFormat) (*Wrapper, error) {
 		QueryWrapper: QueryWrapper{
 			db:                conn,
 			placeholderFormat: placeholder,
+			RetryCount:        3,
 		},
 	}, nil
 }
@@ -79,26 +100,6 @@ func (w Wrapper) Transact(ctx context.Context, opts *sql.TxOptions, cb func(cont
 	return tx.Commit()
 }
 
-func (w QueryWrapper) SelectRow(ctx context.Context, bb *sq.SelectBuilder) *Row {
-	statement, params, err := bb.PlaceholderFormat(w.placeholderFormat).ToSql()
-	if err != nil {
-		return &Row{
-			err: err,
-		}
-	}
-
-	rows, err := w.db.QueryContext(ctx, statement, params...)
-	if err != nil {
-		return &Row{
-			err: err,
-		}
-	}
-
-	return &Row{
-		Rows: rows,
-	}
-}
-
 func (w QueryWrapper) Insert(ctx context.Context, bb *sq.InsertBuilder) (sql.Result, error) {
 	statement, params, err := bb.PlaceholderFormat(w.placeholderFormat).ToSql()
 	if err != nil {
@@ -111,7 +112,10 @@ func (w QueryWrapper) Insert(ctx context.Context, bb *sq.InsertBuilder) (sql.Res
 func (w QueryWrapper) ExecRaw(ctx context.Context, statement string, params ...interface{}) (sql.Result, error) {
 	res, err := w.db.ExecContext(ctx, statement, params...)
 	if err != nil {
-		return nil, err
+		return nil, &QueryError{
+			cause:     err,
+			Statement: statement,
+		}
 	}
 	return res, nil
 }
@@ -133,6 +137,7 @@ func (w QueryWrapper) Update(ctx context.Context, bb *sq.UpdateBuilder) (sql.Res
 	return w.db.ExecContext(ctx, statement, params...)
 }
 
+// Select runs a builder to query, returning Rows. Transient errors will be retried. Do not modify data in a select.
 func (w QueryWrapper) Select(ctx context.Context, bb *sq.SelectBuilder) (*Rows, error) {
 	statement, params, err := bb.PlaceholderFormat(w.placeholderFormat).ToSql()
 
@@ -141,9 +146,37 @@ func (w QueryWrapper) Select(ctx context.Context, bb *sq.SelectBuilder) (*Rows, 
 	}
 
 	return w.QueryRaw(ctx, statement, params...)
-
 }
 
+// SelectRow returns a single row, otherwise is the same as Select
+func (w QueryWrapper) SelectRow(ctx context.Context, bb *sq.SelectBuilder) *Row {
+	rows, err := w.Select(ctx, bb)
+	if err != nil {
+		return &Row{
+			err: err,
+		}
+	}
+
+	return &Row{
+		Rows: rows,
+	}
+}
+
+// SelectRaw runs a string + params query, with automatic retry on transient errors
+func (w QueryWrapper) SelectRaw(ctx context.Context, statement string, params ...interface{}) (*Rows, error) {
+	var rows *Rows
+	var err error
+	for tries := 0; tries < w.RetryCount; tries++ {
+		rows, err = w.QueryRaw(ctx, statement, params...)
+		if err == nil {
+			break
+		}
+	}
+	return rows, err
+}
+
+// QueryRaw runs a query directly with the driver, returning wrapped rows. It
+// will not attempt to retry. Use SelectRaw for automatic retries
 func (w QueryWrapper) QueryRaw(ctx context.Context, statement string, params ...interface{}) (*Rows, error) {
 	rows, err := w.db.QueryContext(ctx, statement, params...)
 	if err != nil {
