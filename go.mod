d6cc3e1f03c835d8c0e633557c07bee812c94057
Damien Whitten <dw@chaos404.com>
2025-01-07 15:10:14 -0800

    Add Opener

--- go.mod
+++ go.mod
@@ -1,9 +1,19 @@
 module github.com/pentops/sqrlx.go
 
-go 1.21
+go 1.22.0
+
+toolchain go1.23.4
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/elgris/sqrl v0.0.0-20210727210741-7e0198b30236
 	github.com/lib/pq v1.10.3
+	github.com/pentops/log.go v0.0.14
+)
+
+require (
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
---
+++ pgenv/pgenv.go
@@ -0,0 +1,49 @@
+package pgenv
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/pentops/log.go/log"
+	"github.com/pentops/sqrlx.go/sqrlx"
+)
+
+type DatabaseConfig struct {
+	URL          string `env:"POSTGRES_URL"`
+	MaxOpenConns int    `env:"POSTGRES_MAX_OPEN_CONNS" default:"10"`
+}
+
+func (cfg *DatabaseConfig) OpenPostgres(ctx context.Context) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+
+	for {
+		if ctx.Err() != nil {
+			return nil, context.Canceled
+		}
+		if err := db.PingContext(ctx); err != nil {
+			log.WithError(ctx, err).Error("pinging PG")
+			time.Sleep(time.Second)
+			continue
+		}
+		break
+	}
+
+	log.Info(ctx, "connected to PG")
+
+	return db, nil
+}
+
+func (cfg *DatabaseConfig) OpenPostgresTransactor(ctx context.Context) (sqrlx.Transactor, error) {
+	db, err := cfg.OpenPostgres(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return sqrlx.NewPostgres(db), nil
+}
